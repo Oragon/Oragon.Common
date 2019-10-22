2989b5e2402a233241498dbb8335986d07d5786e
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2019-10-22 04:48:32 -0300

    Default Implementation with reference for another implementation (async vs sync)

---
+++ Oragon.Common/Configuration/ConfigurationResolverBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oragon.Configuration
+{
+    /// <summary>
+    /// Base implementation for IConfigurationResolver
+    /// </summary>
+    /// <remarks>
+    /// Override GetConfiguration() or GetConfigurationAsync(). 
+    /// Does not implement anyone cause a circular reference and stack overflow
+    /// </remarks>
+    public abstract class ConfigurationResolverBase : IConfigurationResolver
+    {
+        public virtual string GetConfiguration() => this.GetConfigurationAsync().GetAwaiter().GetResult();
+
+        public virtual Task<string> GetConfigurationAsync() => Task.FromResult(this.GetConfiguration());
+    }
+}
